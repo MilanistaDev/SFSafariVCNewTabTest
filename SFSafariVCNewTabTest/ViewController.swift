b4708673376e9690a2ce07031a28df340b2bad8a
Takuya Aso <milanista224@gmail.com>
2018-06-15 13:22:23 +0900

    Initial Commit

---
+++ SFSafariVCNewTabTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SFSafariVCNewTabTest
+//
+//  Created by 麻生 拓弥 on 2018/06/15.
+//  Copyright © 2018年 com.ASTK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
